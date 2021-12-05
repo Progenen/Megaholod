3f9439de4ddef34d78b33ef84a208eee8368f428
John Doe <stanislav_2010@icloud.com>
2021-12-05 11:53:12 +0500

    Fix header

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,2 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    // Burger menu (Header)
+    
+    class MainMenu {
+        constructor() {
+            this.menu = document.querySelector('.main-menu');
+            this.body = document.body;
+            this.menuBtn = document.querySelectorAll('.main-menu-btn');
+        }
+
+        openMenu = (e) => {
+            this.menu.classList.add('show');
+            this.body.classList.add('lock');
+            e.target.classList.add('collapsed');
+        }
+
+        closeMenu = (e) => {
+            this.menu.classList.remove('show');
+            this.body.classList.remove('lock');
+            e.target.classList.remove('collapsed');
+        }
+
+        render () {
+            this.menuBtn.forEach(element => {
+                element.addEventListener('click', (e) => {
+                    if (this.menu.classList.contains('show')) {
+                        this.closeMenu(e);
+                    } else {
+                        this.openMenu(e);
+                    }
+                })
+            })
+        }
+    }
+
+    const mainMenuInstance = new MainMenu();
+    mainMenuInstance.render();
+
 });
